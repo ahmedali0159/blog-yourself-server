2ff046ebcd0ba402b994c83b946f74e8cdc56316
ahmedali0159 <pirates0159@gmail.com>
2021-06-26 18:00:59 +0600

    add collection

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("blogYourself!");
+});
+
+
+const { MongoClient } = require('mongodb');
+const uri = ` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g3qco.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+    console.log('connection err',err);
+  const postCollection = client.db("blog-yourself").collection("post");
+  console.log('database connected successfully')
+
+  app.get('/events', (req, res)=> {
+      postCollection.find()
+      .toArray((err, items) => {
+          res.send(items)
+          console.log('from database', items);
+      })
+  })
+
+  app.post("/addEvent", (req, res) => {
+    const newEvent = req.body;
+    console.log("adding new event", newEvent);
+    postCollection.insertOne(newEvent).then((result) => {
+        console.log("inserted count", result.insertedCount)
+        res.send(result.insertedCount > 0);
+    });
+  });
+
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
